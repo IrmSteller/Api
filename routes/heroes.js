8db21174b28dc0e81f13e036ee68312b7aef0d67
IrmSteller <IrmStellerqgmx.de>
2020-07-21 21:37:21 +0200

    mit mongo und docker

--- routes/heroes.js
+++ routes/heroes.js
@@ -1,15 +1,35 @@
 const heroRoutes = (app, fs) => {
     const dataPath = "./data/heroes.json";
+    const Hero = require('../models/Hero');
+    app.get('/heroes',(req, res) => {
+        Hero.find().then(heroes => res.json(heroes)).catch(err => console.log("not found"));
+    
+    });
 
-    app.get('/heros',(req, res) => {
-        fs.readFile(dataPath, "utf8", (err, data) => {
-        if (err) {
-            console.log(err);
-            throw err;
-        }
+    app.get('/heros/:id', (req,res) =>{
+        Hero.findOne({id: req.params.id})
+    });
 
-        res.send(JSON.parse(data));
+    app.put('/heroes/:id', (req, res) => {
+        var hero = Hero.findOneAndUpdate(
+            {id: req.params.id},
+            {$set:{   name: req.body.name}},
+            {upsert: true}
+        ).then(res.send("OK")).catch(res.send("NOK"));
+    });
+
+    app.delete("/heroes/:id", (req,res) =>{
+        Hero.deleteOne({id : req.params})
+        .then(res.send("Ok"))
+        .catch(err => res.send("not found"))
+    });
+    app.post('/heroes',(req, res) => {
+        console.log(req.body.id);
+        const newHero = new Hero({
+            id: req.body.id,
+            name: req.body.name
         });
+        newHero.save().then(res.json("OK")).catch("NOK");
     });
 };
 
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyparser = require("body-parser");
 const cors = require("cors");
-
+const mongoose = require('mongoose');
 const app = express();
 const fs = require("fs");
 
@@ -9,8 +9,12 @@ app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended: true}));
 app.use(cors());
 
+mongoose.connect('mongodb://mongo:27017/heros',
+    {useNewUrlParser: true}
+).then(()=> console.log('MongoDb connected')).catch(err => console.log(err));
+
 const routes = require('./routes/routes.js')(app, fs);
 
-const server = app.listen(3001,()=>{
+const server = app.listen(3000,()=>{
     console.log(`listing on port ${server.address().port}`);
 })
